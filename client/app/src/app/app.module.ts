84e9324a0cf32c01f3ac09ab35e21cdddba9692d
marlewandowski <maniszap@gmail.com>
2022-02-23 20:37:10 +0100

    Deleted old components, navbar update

--- client/app/src/app/app.module.ts
+++ client/app/src/app/app.module.ts
@@ -9,14 +9,8 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { InfoComponent } from './info/info.component';
 import { EventListComponent } from './events/event-list/event-list.component';
-import { ResultsComponent } from './results/results.component';
-import { RulesComponent } from './rules/rules.component';
 import { DriversComponent } from './drivers/drivers.component';
-import { TeamsComponent } from './teams/teams.component';
-import { PenaltiesComponent } from './penalties/penalties.component';
-import { StandingsComponent } from './standings/standings.component';
 import { EventBoxComponent } from './events/event-box/event-box.component';
 import { JwtInterceptor } from '_interceptors/jwt.interceptor';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -25,8 +19,8 @@ import { RaceBoxComponent } from './races/race-box/race-box.component';
 import { RaceDetailsComponent } from './races/race-details/race-details.component';
 import { DriverListComponent } from './drivers/driver-list/driver-list.component';
 import { DriverCardComponent } from './drivers/driver-card/driver-card.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { EventRulesComponent } from './events/event-rules/event-rules.component';
 import { EventTeamsComponent } from './events/event-teams/event-teams.component';
 import { EventPenaltyComponent } from './events/event-penalty/event-penalty.component';
@@ -39,14 +33,8 @@ import { EventInfoComponent } from './events/event-info/event-info.component';
     NavComponent,
     HomeComponent,
     RegisterComponent,
-    InfoComponent,
     EventListComponent,
-    ResultsComponent,
-    RulesComponent,
     DriversComponent,
-    TeamsComponent,
-    PenaltiesComponent,
-    StandingsComponent,
     EventBoxComponent,
     EventDetailsComponent,
     RaceBoxComponent,
