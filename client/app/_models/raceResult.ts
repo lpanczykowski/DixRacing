36f7d46b4ba6276cbf03ee61f741dcbc6c51641d
marlewandowski <maniszap@gmail.com>
2022-04-03 12:37:15 +0200

    Race results table added

---
+++ client/app/_models/raceResult.ts
@@ -0,0 +1,17 @@
+export interface RaceResult {
+  DriverNumber: number;
+  DriverName: string;
+  DriverSurname: string;
+  TeamName: string;
+  Car: number;
+  RaceId: number;
+  Position: number;
+  Sector1Time:number;
+  Sector2Time:number;
+  Sector3Time:number;
+  Time: number;
+  Gap: number;
+  Laps: number;
+  Points: number;
+  PenaltyPoints: number;
+}
--- client/app/src/app/races/race-results/race-results.component.ts
+++ client/app/src/app/races/race-results/race-results.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatCardActions } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { RaceResult } from '_models/raceResult';
+import { RaceService } from '_services/race.service';
+
 
 @Component({
   selector: 'app-race-results',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./race-results.component.css']
 })
 export class RaceResultsComponent implements OnInit {
+  @Input() resultsSelector: string;
+  currentResults: string;
+  raceId:number;
+  raceResults: RaceResult[] = [{ DriverNumber: 830, DriverName: 'Marcin', DriverSurname: 'Lewandowski',TeamName:'SMR Racing', Car:1, Sector1Time:39, Sector2Time: 68, Sector3Time:27, Laps: 50, PenaltyPoints: 0, Points: 100, Position: 1, RaceId: 1, Time: 1995, Gap:0 },
+                                { DriverNumber: 730, DriverName: 'Marcin', DriverSurname: 'Lewandowski',TeamName:'SMR Racing', Car:1, Sector1Time:35, Sector2Time: 59, Sector3Time:21, Laps: 50, PenaltyPoints: 0, Points: 90, Position: 2, RaceId: 1, Time: 1990, Gap:5 },
+                                { DriverNumber: 930, DriverName: 'Marcin', DriverSurname: 'Lewandowski',TeamName:'SMR Racing', Car:1, Sector1Time:31, Sector2Time: 61, Sector3Time:28, Laps: 50, PenaltyPoints: 5, Points: 80, Position: 3, RaceId: 1, Time: 1980, Gap:15 }];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private raceService: RaceService) { }
 
   ngOnInit(): void {
+    this.raceId = Number(this.route.snapshot.paramMap.get('raceId'));
+    //this.loadView();
   }
 
+  loadView() {
+    this.raceService.getRaceResults(this.raceId,this.resultsSelector);
+  }
 }
