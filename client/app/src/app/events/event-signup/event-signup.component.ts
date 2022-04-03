b08bc5fa30687210584ca4a54611559aac23b546
marlewandowski <maniszap@gmail.com>
2022-04-03 12:36:15 +0200

    event signup form

---
+++ client/app/src/app/events/event-signup/event-signup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { EventCar } from '_models/eventCar';
+import { EventTeam } from '_models/eventTeam';
+
+@Component({
+  selector: 'app-event-signup',
+  templateUrl: './event-signup.component.html',
+  styleUrls: ['./event-signup.component.css']
+})
+export class EventSignupComponent implements OnInit {
+  constructor() { }
+  eventCars: EventCar[]=[{car:1},{car:2},{car:3}];
+  eventTeams: EventTeam[]=[{teamName:'zurek'},{teamName:'nowyskoski'},{teamName:'teamname'}]
+  ngOnInit(): void {
+  }
+
+  eventSignup()
+  {
+
+  }
+
+  getCarsForEvent()
+  {
+
+  }
+
+  createNewTeam()
+  {
+
+  }
+}
