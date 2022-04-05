c09864e587b071659533c22bd283ee64d79565ed
Łukasz Panczykowski <lukasz.panczykowski@proman.com.pl>
2022-04-05 17:51:54 +0200

    path fix

--- client/app/src/app/events/event-teams/event-teams.component.ts
+++ client/app/src/app/events/event-teams/event-teams.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Team } from 'src/app/_models/team';
-import { EventService } from 'src/app/_services/event.service';
+import { Team } from 'app/_models/team';
+import { EventService } from 'app/_services/event.service';
 
 @Component({
   selector: 'app-event-teams',
   templateUrl: './event-teams.component.html',
-  styleUrls: ['./event-teams.component.css']
+  styleUrls: ['./event-teams.component.css'],
 })
 export class EventTeamsComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventService
+  ) {}
 
-  constructor(private route:ActivatedRoute, private eventService:EventService) { }
-
-  eventId:number;
-  teams:Team[]=[{teamId:1,teamName:'team1',teamMate:['LU KAS','MA TI'],teamCar:2,teamPoints:50},
-                {teamId:2,teamName:'team2',teamMate:['RO BOT','BUL BAN','Robert Maklowicz'],teamCar:8,teamPoints:40}];
+  eventId: number;
+  teams: Team[] = [
+    {
+      teamId: 1,
+      teamName: 'team1',
+      teamMate: ['LU KAS', 'MA TI'],
+      teamCar: 2,
+      teamPoints: 50,
+    },
+    {
+      teamId: 2,
+      teamName: 'team2',
+      teamMate: ['RO BOT', 'BUL BAN', 'Robert Maklowicz'],
+      teamCar: 8,
+      teamPoints: 40,
+    },
+  ];
 
   ngOnInit(): void {
-
-    this.eventId=Number(this.route.snapshot.paramMap.get('eventId'));
+    this.eventId = Number(this.route.snapshot.paramMap.get('eventId'));
     //getTeamsForEvent(this.eventId);
   }
 
-  getTeamsForEvent(eventId){
+  getTeamsForEvent(eventId) {
     this.eventService.getTeamsForEvent(eventId);
   }
 }
